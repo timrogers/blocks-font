75f9f0eecc611c232a1847fe70e4d2d743d92355
Tim Rogers <me@timrogers.co.uk>
2022-05-12 16:09:00 +0100

    Fix font loading and add size, weight and text customisation

--- src/blocks/font-block/index.tsx
+++ src/blocks/font-block/index.tsx
@@ -1,11 +1,20 @@
 import { FileBlockProps } from "@githubnext/utils";
-import { Box } from "@primer/react";
+import { Box, FormControl, Select } from "@primer/react";
 import { useEffect, useState } from "react";
 
+const numbersFromOneToN = (n: number): number[] => {
+  return [...Array(n).keys()].map((n) => n + 1);
+};
+
+const DEFAULT_PREVIEW_TEXT = "The quick brown fox jumps over the lazy dog";
 const SUPPORTED_EXTENSIONS = ["ttf"];
+const SUPPORTED_FONT_WEIGHTS = ["normal", "bold"];
+const SUPPORTED_FONT_SIZES = numbersFromOneToN(100);
 
 const FontPreviewComponent = (props: { filename: string; url: string }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [fontSizeInPx, setFontSizeInPx] = useState<number>(32);
+  const [fontWeight, setFontWeight] = useState<string>("normal");
 
   const { filename, url } = props;
 
@@ -21,15 +30,7 @@ const FontPreviewComponent = (props: { filename: string; url: string }) => {
   if (isLoading) {
     return (
       <Box p={4}>
-        <Box
-          borderColor="border.default"
-          borderWidth={1}
-          borderStyle="solid"
-          borderRadius={6}
-          overflow="hidden"
-        >
-          Loading font...
-        </Box>
+        <Box>Loading font...</Box>
       </Box>
     );
   } else {
@@ -37,13 +38,55 @@ const FontPreviewComponent = (props: { filename: string; url: string }) => {
       <Box p={4}>
         <Box
           borderColor="border.default"
-          borderWidth={1}
           borderStyle="solid"
+          borderWidth={2}
           borderRadius={6}
+          display="grid"
+          gridGap={3}
           overflow="hidden"
+          p={4}
         >
-          <span style={{ fontFamily: `'"${filename}"` }}>
-            The quick brown fox jumped over the lazy dog
+          <FormControl>
+            <FormControl.Label>Font size</FormControl.Label>
+            <Select
+              value={fontSizeInPx.toString()}
+              onChange={(event) =>
+                setFontSizeInPx(parseInt(event.currentTarget.value))
+              }
+            >
+              {SUPPORTED_FONT_SIZES.map((fontSize) => (
+                <Select.Option value={fontSize.toString()}>
+                  {fontSize}px
+                </Select.Option>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <FormControl.Label>Font weight</FormControl.Label>
+            <Select
+              value={fontWeight}
+              onChange={(event) => setFontWeight(event.currentTarget.value)}
+            >
+              {SUPPORTED_FONT_WEIGHTS.map((fontWeight) => (
+                <Select.Option value={fontWeight.toString()}>
+                  {fontWeight}
+                </Select.Option>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Box textAlign="center" m={4}>
+          <span
+            contentEditable
+            style={{
+              fontFamily: `"${filename}"`,
+              fontSize: `${fontSizeInPx}px`,
+              fontWeight,
+            }}
+          >
+            {DEFAULT_PREVIEW_TEXT}
           </span>
         </Box>
       </Box>
@@ -57,12 +100,13 @@ export default function (props: FileBlockProps) {
   const filename = Boolean(context.file)
     ? context.file.split(".").shift()
     : undefined;
+
   const extension = Boolean(context.path)
     ? context.path.split(".").pop()
     : undefined;
 
   if (filename && extension && SUPPORTED_EXTENSIONS.includes(extension)) {
-    const url = `https://github.com/${context.owner}/${context.repo}/blob/${context.sha}/${context.path}?raw=true`;
+    const url = `https://cdn.jsdelivr.net/gh/${context.owner}/${context.repo}@${context.sha}/${context.path}`;
 
     return <FontPreviewComponent url={url} filename={filename} />;
   } else {
